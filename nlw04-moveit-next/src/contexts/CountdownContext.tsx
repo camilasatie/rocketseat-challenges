d413f5517a8ac8804fea5f1ad86ee73b68bb79a6
Camila <camilasatie.dev@gmail.com>
2021-03-07 14:40:42 -0300

    feat: add notification

--- nlw04-moveit-next/src/contexts/CountdownContext.tsx
+++ nlw04-moveit-next/src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export const CountdownContext = createContext({} as CountdownContextData);
 
 export function CountdownProvider({ children }: CountdownProviderProps) {  
-  const {startNewChallenge } = useContext(ChallengesContext);
+  const { startNewChallenge } = useContext(ChallengesContext);
   
   const [time, setTime] = useState(MINUTES_PER_CYCLE * ONE_MINUTE_IN_SECOND);
   const [isActive, setIsActive] = useState(false);
