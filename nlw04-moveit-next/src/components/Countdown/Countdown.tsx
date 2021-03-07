bb91485d027ed698b7f32c6c836736e9644d9ea5
Camila <camilasatie.dev@gmail.com>
2021-03-07 10:29:49 -0300

    feat: add global const

--- nlw04-moveit-next/src/components/Countdown/Countdown.tsx
+++ nlw04-moveit-next/src/components/Countdown/Countdown.tsx
@@ -2,46 +2,50 @@ import { useState, useEffect } from 'react';
 
 import styles from './Countdown.module.css';
 
+const MINUTES_PER_CYCLE = 0.1;
+const ONE_MINUTE_IN_SECOND = 60;
 let countdownTimeout: NodeJS.Timeout;
 
 function Countdown() {
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(MINUTES_PER_CYCLE * ONE_MINUTE_IN_SECOND);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const minutes = Math.floor(time / ONE_MINUTE_IN_SECOND);
+  const seconds = time % ONE_MINUTE_IN_SECOND;
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   const startCountDown = () => {
     setIsActive(true);
-  }
+  };
 
   const resetCountdown = () => {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
-  }
+    setTime(MINUTES_PER_CYCLE * ONE_MINUTE_IN_SECOND);
+  };
 
   const renderButton = () => {
     if(hasFinished) {
       return (
         <button disabled className={styles.countdownButton}>
-          Ciclo Encerrado        
+          Ciclo Encerrado
+          <img src="icons/check_circle.svg" alt="" />        
         </button>
       );
-    } 
+    }; 
   
     if(isActive) {
       return (
         <button 
-          onClick={resetCountdown} 
-          type="button" 
-          className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+        type="button" 
+        className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+        onClick={resetCountdown} 
         >
           Abandonar ciclo
+          <img src="icons/close.svg" alt="" />
         </button>
       );  
     } else {
@@ -50,8 +54,8 @@ function Countdown() {
           Iniciar ciclo
         </button>
       );
-    }
-  }
+    };
+  };
 
   useEffect(() => {
     if (isActive && time > 0) {
@@ -61,7 +65,7 @@ function Countdown() {
     } else if (isActive && time === 0) {
       setHasFinished(true);
       setIsActive(false);
-    }
+    };
   }, [isActive, time]);
 
   return (
@@ -81,7 +85,7 @@ function Countdown() {
       </div>
       {renderButton()}      
     </div>
-  )
+  );
 };
 
 export default Countdown;
